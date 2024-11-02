2eabb14113296ec73b12fe303a6d5a1907b7cf8c
gxw <guxiwei-hf@loongson.cn>
2024-11-02 17:06:15 +0800

    LoongArch: Add Copyright

--- Eigen/src/Core/arch/LSX/PacketMath.h
+++ Eigen/src/Core/arch/LSX/PacketMath.h
@@ -2,6 +2,7 @@
 // for linear algebra.
 //
 // Copyright (C) 2023 Zang Ruochen <zangruochen@loongson.cn>
+// Copyright (C) 2024 XiWei Gu <guxiwei-hf@loongson.cn>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
